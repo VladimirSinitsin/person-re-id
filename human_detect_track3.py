2a7b410dd9730b8bced290fd6fdfc5cdee40d275
VladimirSinitsin <vladimir.sinitsin.ya@yandex.ru>
2021-06-08 06:54:46 +0300

    Разные цвета в отрисовке.
    - удалены комментарии.

--- human_detect_track3.py
+++ human_detect_track3.py
@@ -1,30 +1,27 @@
-"""
-Version3
-tracking + reidentification.
-Tracking is done using last k=15 frames.
-
-Change from human_detect_track3: some changes in find()
-Used matchTemplate() to check the result of tracking.
-Sometimes, 2 people very close to each other are tracked as
-the same person, so this makes sure that this error does not occur.
-"""
 import numpy as np
 import tensorflow as tf
 import cv2
 import time
 import shutil
 import os
+import random
 
 from pathlib import Path
 from PIL import ImageFont, ImageDraw, Image
 from datetime import datetime
 
 from run import Reid
-# from run import main2
-from importlib import import_module
 
+
+random.seed(111)
 
 SCRIPT_PATH = str(Path(__file__).parent.absolute())
+video_file_name = 'video2.avi'
+# video_file_name = 'me.mp4'
+colors = {0: (0, 255, 0),
+          1: (38, 0, 237),
+          2: (143, 39, 145),
+          3: (0, 157, 250)}
 
 
 class DetectorAPI:
@@ -41,34 +38,25 @@ class DetectorAPI:
 
         self.default_graph = self.detection_graph.as_default()
         self.sess = tf.Session(graph=self.detection_graph)
-
-        # Definite input and output Tensors for detection_graph
         self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-        # Each box represents a part of the image where a particular object was detected.
         self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
         self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
         self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
         self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
     def process_frame(self, image):
-        # Expand dimensions since the trained_model expects images to have shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(image, axis=0)
-        # Actual detection.
-        start_time = time.time()
         (boxes, scores, classes, num) = self.sess.run(
                 [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
                 feed_dict={self.image_tensor: image_np_expanded})
-        end_time = time.time()
 
         im_height, im_width, _ = image.shape
         boxes_list = [None for _ in range(boxes.shape[1])]
         for i in range(boxes.shape[1]):
-            boxes_list[i] = (
-            int(boxes[0, i, 0] * im_height), int(boxes[0, i, 1] * im_width), int(boxes[0, i, 2] * im_height),
-            int(boxes[0, i, 3] * im_width))
-
+            boxes_list[i] = (int(boxes[0, i, 0] * im_height),
+                             int(boxes[0, i, 1] * im_width),
+                             int(boxes[0, i, 2] * im_height),
+                             int(boxes[0, i, 3] * im_width))
         return boxes_list, scores[0].tolist(), [int(x) for x in classes[0].tolist()], int(num[0])
 
     def close(self):
@@ -76,7 +64,6 @@ class DetectorAPI:
         self.default_graph.close()
 
     def find(self, img, boxes_cur, boxes_prev, box):
-        # print('## Find called')
         cv2.imwrite('./temporaryImg.jpg', img)
 
         past_ppl = './past_ppl'
@@ -115,9 +102,8 @@ class DetectorAPI:
 
 def make_empty_folder(out_path):
     print(out_path)
-    # make path if not exists
     if not os.path.exists(out_path):
-        os.makedirs(out_path)  # empty it if anything there
+        os.makedirs(out_path)
         return
     shutil.rmtree(out_path)
     make_empty_folder(out_path)
@@ -141,37 +127,39 @@ def iou(box1, box2):
     box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
     box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
 
-    # compute the intersection over union by taking the intersection
-    # area and dividing it by the sum of prediction + ground-truth
-    # areas - the interesection area
     iou = float(inter_area) / float(box1_area + box2_area - inter_area)
-
-    # return the intersection over union value
     return iou
 
 
+def new_color():
+    global colors
+    new_color = (random.randint(20, 230), random.randint(20, 230), random.randint(20, 230))
+    colors_vales = colors.values()
+    while new_color in colors_vales:
+        new_color = (random.randint(20, 230), random.randint(20, 230), random.randint(20, 230))
+    colors[len(colors)] = new_color
+    return new_color
+
+
 def draw_text(img, box, num):
+    num = int(num)
     text_to_draw = f'человек {num}'
     font_w, font_h = FONT.getsize(text_to_draw)
     y_min, x_min, y_max, x_max = box
     xy_coords = (x_min, y_min - font_h - 2)
 
-    cv2.rectangle(img, (x_min - 1, y_min - 1), (x_max + 1, y_max + 1), (0, 255, 0), 2)
-    cv2.rectangle(img, (x_min - 1, y_min - font_h - 1), (x_min + font_w + 1, y_min), (0, 255, 0), -1)
+    color = colors[num] if num in colors.keys() else new_color()
+    cv2.rectangle(img, (x_min - 1, y_min - 1), (x_max + 1, y_max + 1), color, 2)
+    cv2.rectangle(img, (x_min - 1, y_min - font_h - 1), (x_min + font_w + 1, y_min), color, -1)
 
     img_pil = Image.fromarray(img)
     draw = ImageDraw.Draw(img_pil)
     draw.text((xy_coords[0] + 1, xy_coords[1] + 1), text_to_draw, font=FONT, fill=(0, 0, 0))
     draw.text(xy_coords, text_to_draw, font=FONT, fill=(255, 255, 255))
     img = np.array(img_pil)
-
-    # cv2.imwrite(f'{SCRIPT_PATH}/{records}/{count}.jpg', img)
-    # count += 1
-
     return img
 
 
-#   model_path = '/path/to/faster_rcnn_inception_v2_coco_2017_11_08/frozen_inference_graph.pb'
 FONT = ImageFont.truetype('./Fallout2Cyr.ttf', 16)
 model_path = './model/frozen_inference_graph.pb'
 
@@ -184,18 +172,15 @@ make_empty_folder(SCRIPT_PATH + records)
 odapi = DetectorAPI(path_to_ckpt=model_path)
 threshold = 0.8
 iou_threshold = 0.6
-cap = cv2.VideoCapture('./video2.avi')
+cap = cv2.VideoCapture(f'./{video_file_name}')
 
-# maximum number of previous frames to check iou with
 k = 15
 
-# this will store the bounding boxes detected in the previous frame.
 boxes_prev = []
 framenum = 1
 start_time = time.time()  # seconds
 time240 = 0
 
-# iterate over frames
 while True:
     _, source_img = cap.read()
     img = cv2.resize(source_img, (1280, 720))
@@ -206,29 +191,22 @@ while True:
         boxes_cur.append([-1] + box[:k - 1])
 
     for i in range(len(boxes)):
-        # Class 1 represents human
         if classes[i] == 1 and scores[i] > threshold:
             box = boxes[i]
             y_min, x_min, y_max, x_max = box
 
-            # draw the bounding box on the image
-            # cv2.rectangle(img, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
-
             cropped_img = img[y_min:y_max, x_min:x_max]
 
             maxthreshold = -1
-            maxindex = 101  # the index in boxes_prev indicating the matching person from the previous k frames.
+            maxindex = 101
             maxframe = -1
 
             for j in range(len(boxes_prev)):
-                # Every boxes_prev[j] denotes a person. It is a list of the last k positions of the person j.
-
-                # This previous person has already been alloted to another person in the current frame
                 if boxes_prev[j] == -1:
                     continue
 
                 for kk in range(len(boxes_prev[j])):
-                    if boxes_prev[j][kk] == -1:  # person was not detected in frame kk
+                    if boxes_prev[j][kk] == -1:
                         continue
                     curr_iou = iou(boxes_prev[j][kk], box)
                     if curr_iou > maxthreshold and curr_iou > iou_threshold:
@@ -237,7 +215,6 @@ while True:
                         maxframe = kk
 
             if maxthreshold != -1:
-                # Was the tracking correct?
                 b = boxes_prev[maxindex][maxframe]
                 old_img = img[b[0]:b[2], b[1]:b[3]]
                 cur_img = cropped_img
@@ -247,27 +224,20 @@ while True:
 
                 res = cv2.matchTemplate(old_img, cur_img, cv2.TM_CCOEFF_NORMED)
                 if res[0][0] < 0.45:
-                    # Tracking was incorrect
                     maxthreshold = -1
 
-            # maxthreshold != -1 at this point means this person is the same as prevbox in the last frame.
             if maxthreshold != -1:
-                # print('tracked ###########')
                 boxes_cur[maxindex][0] = box
                 boxes_prev[maxindex] = -1
 
-                # also add this image of the person to his previous images
                 person_no = len(os.listdir(SCRIPT_PATH + past_ppl + '/' + str(maxindex))) + 1
                 cv2.imwrite(SCRIPT_PATH + past_ppl + '/' + str(maxindex) + '/' + str(person_no) + '.jpg', cropped_img)
                 img = draw_text(img, box, maxindex)
             else:
-                # The person was not present in the previous frame. Add him to a new folder.
-                # The folder name should be equal to the index of the person in box_cur.
                 person_no = odapi.find(cropped_img, boxes_cur, boxes_prev, box)
                 img = draw_text(img, box, person_no)
 
     num_ppl = len(os.listdir(SCRIPT_PATH + past_ppl))
-    # print('#People:   ' + str(num_ppl))
     print('Time for ' + str(framenum) + ' frames: (seconds)')
     print(time.time() - start_time)
     print('\n')
